03af4241fade55e9c9f7051d409455f1abc6e3c6
Android <hyliozqq@gmail.com>
2018-10-22 18:31:30 +0300

    Add annotations

--- src/main/java/entities/Car.java
+++ src/main/java/entities/Car.java
@@ -3,21 +3,27 @@ package entities;
 import enums.CarTypeEnum;
 import enums.ManufacturerEnum;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.Year;
 
 /**
  * Entity car
  */
 abstract public class Car {
+    @NotNull(message = "Manufacturer must be set")
     private ManufacturerEnum manufacturer;
     private String model;
     private CarTypeEnum bodyType;
-
     private String engineType;
-    private int maxSpeed;
-    private int price;
 
+    @Size(min = 50, max = 400, message = "Max speed should be in range(50, 400)")
+    private int maxSpeed;
+    @Size(min = 100, max = 400000, message = "Price should be in range(100, 400000)")
+    private int price;
+    @Size(min = 1950, max = 2018, message = "Year should be in range(1950, 2018)")
     private Year year;
+    @Size(min = 2, max = 10, message = "Number of seats should be in range(2, 10)")
     private int numberOfSeats;
     private boolean automaticTransmission;
 
